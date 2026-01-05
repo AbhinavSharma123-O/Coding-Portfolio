f043f1e213c14fe20c33bc9cc2eec7e1336f8a4d
AbhinavSharma123-O <abhinavprofessional123@gmail.com>
2026-01-05 19:20:03 +0530

    Add files via upload

---
+++ Const/Const/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int vat = 20;
+           
+            int balance = 1000;
+            
+            Console.WriteLine(balance * (vat / 10));
+            Console.WriteLine(vat);
+            Console.ReadLine();
+        }
+    }
+}
