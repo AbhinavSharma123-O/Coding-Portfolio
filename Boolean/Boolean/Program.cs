8566039647db2ea1d28138bb3ea29ee7a991a519
AbhinavSharma123-O <abhinavprofessional123@gmail.com>
2026-01-05 19:18:40 +0530

    Add files via upload

---
+++ Boolean/Boolean/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value = false;
+            bool isMale = true;
+            Console.WriteLine(isMale);
+            Console.WriteLine(value);
+            isMale = false;
+            Console.WriteLine(isMale);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Boolean/Boolean/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Boolean")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Boolean")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34b6b6b6-0a37-4390-91e8-451e065edd3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
