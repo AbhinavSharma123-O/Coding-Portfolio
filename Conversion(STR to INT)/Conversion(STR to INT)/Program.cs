2447244fcbf576af03de1f49ba420b80921d1a68
AbhinavSharma123-O <abhinavprofessional123@gmail.com>
2026-01-06 20:50:58 +0530

    Add files via upload

---
+++ Conversion(STR to INT)/Conversion(STR to INT)/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion_STR_to_INT_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textage = "78";
+            int age = Convert.ToInt32(textage);
+            Console.WriteLine(age);
+
+        }
+    }
+}
