e83a79dc7c73cacbd5c27609b88a85551061a2f3
AbhinavSharma123-O <abhinavprofessional123@gmail.com>
2026-01-08 01:08:42 +0530

    Add files via upload

---
+++ Excer - 02/Excer - 02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excer___02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 28;
+            int rem;
+            rem = num % 2;
+            if (rem == 0)
+            {
+                Console.WriteLine(num,"is even");
+            }
+            else {
+                Console.WriteLine(num,"is odd");
+        }
+            Console.ReadLine();
+    }
+}
